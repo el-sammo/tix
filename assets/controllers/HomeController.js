41f601785db89849121c69fb07e9793f12d04e36
Basic <sam.barrett@gmail.com>
2016-03-08 12:31:28 -0500

    successfully re-ordered hotReservations

--- assets/controllers/HomeController.js
+++ assets/controllers/HomeController.js
@@ -44,24 +44,58 @@
 							var poolHotReservationData = {
 								id:  pool.id,
 								name: pool.name,
+								eligibleEntities: pool.eligibleEntities,
 								hotReservations: [],
 							};
 							var getHotReservationsPromise = reservationMgmt.getHotReservations(pool.id);
 							getHotReservationsPromise.then(function(hotReservationsData) {
 								if(typeof hotReservationsData !== 'undefined') {
 									hotReservationsData.forEach(function(hotReservation) {
-										var thisHotReservation = {
-											id: hotReservation[0].id,
-											name: hotReservation[0].name,
-											mascot: hotReservation[0].mascot,
-											leagueCode: hotReservation[0].leagueCode,
-											color1: hotReservation[0].color1,
-											color2: hotReservation[0].color2,
-										};
-										if(hotReservation[0].color3) {
-											thisHotReservation.color3 = hotReservation[0].color3;
-										}
-										poolHotReservationData.hotReservations.push(thisHotReservation);
+
+										var expectedOdds = 0;
+										poolHotReservationData.eligibleEntities.forEach(function(eligibleEntity) {
+											if(eligibleEntity.entityId === hotReservation[0].id) {
+												expectedOdds = eligibleEntity.expectedOdds;
+											}
+										});
+
+										var eeCount = poolHotReservationData.eligibleEntities.length;
+										var getCostByPEPromise = reservationMgmt.getCostByPE(
+											poolHotReservationData.id +'-p&e-'+ 
+											hotReservation[0].id +'-p&e-'+ 
+											expectedOdds +'-p&e-'+ 
+											eeCount
+										);
+										getCostByPEPromise.then(function(peData) {
+											var thisHotReservation = {
+												id: hotReservation[0].id,
+												name: hotReservation[0].name,
+												mascot: hotReservation[0].mascot,
+												leagueCode: hotReservation[0].leagueCode,
+												color1: hotReservation[0].color1,
+												color2: hotReservation[0].color2,
+												cost: peData.nextCost
+											};
+											if(hotReservation[0].color3) {
+												thisHotReservation.color3 = hotReservation[0].color3;
+											}
+											
+											poolHotReservationData.hotReservations.push(thisHotReservation);
+
+											// reorder the "hot" reservations
+											var firstCompare = true;
+											function compare(a,b) {
+												if(parseFloat(a.cost) < parseFloat(b.cost)) {
+													return -1;
+												} else if(parseFloat(a.cost) > parseFloat(b.cost)) {
+													return 1;
+												} else { 
+													return 0;
+												}
+												firstCompare = false;
+											}
+											poolHotReservationData.hotReservations.sort(compare);
+										});
 									});
 								}
 							});
