c9ee1c0f288964fe66603c8f0fdaec64655ffb16
Basic <sam.barrett@gmail.com>
2016-03-21 15:00:21 -0400

    implemented aNet

---
+++ assets/controllers/TosController.js
@@ -0,0 +1,23 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('app');
+
+	///
+	// Controllers: Tos
+	///
+	app.controller('TosController', controller);
+	
+	controller.$inject = [
+		'$scope', '$http', '$routeParams', '$rootScope' 
+	];
+
+	function controller(
+		$scope, $http, $routeParams, $rootScope
+	) {
+
+
+
+	}
+
+}());
